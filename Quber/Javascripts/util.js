fad50de06b09441c52b96b6a4a0bfbe3341c65f0
Kevin Zhang <zhangyongjiang@gmail.com>
2015-07-18 11:43:34 -0700

    listVehicles now works

--- Quber/Javascripts/util.js
+++ Quber/Javascripts/util.js
@@ -6,16 +6,32 @@ function listVehicles() {
     try {
         var div = document.getElementsByClassName("vehicle-selector")[0];
         var ul = searchChildForTag(div, "UL");
+        var lis = searchChildForTags(ul, "LI");
+        var all = '';
+        for(var i=0; i<lis.length; i++) {
+            if(i>0)
+                all += ",";
+            all += lis[i].textContent.trim();
+        }
+        return all;
     } catch (e) {
         alert(e);
     }
 }
 
 function searchChildForTag(node, tagName) {
+    var tags = searchChildForTags(node, tagName);
+    if (tags.length>0)
+        return tags[0];
+}
+
+function searchChildForTags(node, tagName) {
+    var array = [];
     for(var i=0; i<node.childNodes.length; i++) {
         var child = node.childNodes[i];
         if(child.nodeName == tagName) {
-            return child;
+            array.push(child);
         }
     }
+    return array;
 }
